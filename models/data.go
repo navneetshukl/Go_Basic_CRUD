ec87a5296909543d012d319a46039b2bb2723626
Navneet shukla <navneetshukla824@gmail.com>
2023-05-19 23:05:19 +0530

    creating models and some initial data

--- models/data.go
+++ models/data.go
@@ -1,3 +1,17 @@
 package models
 
 
+var Books = []Book{
+    {
+        Id:     1,
+        Title:  "Golang",
+        Author: "Gopher",
+        Desc:   "A book for Go",
+    },
+	{
+		Id: 2,
+		Title: "Data Structures and Algorithms",
+		Author: "Navneet Shukla",
+		Desc: "A book for Data Structures and Algorithms",
+	},
+}
